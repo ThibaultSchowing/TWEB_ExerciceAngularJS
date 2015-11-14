6be3d60f926f7d01aa1d5d5e51bc8273534dac43
Thibault Schowing <thibault.schowing@heig-vd.ch>
2015-11-14 14:31:25 +0100

    base

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 
 
-var express = require('express'),
-  config = require('./config/config');
+var express = require('express');
+
 
 var app = express();
 
-require('./config/express')(app, config);
+// Listening port. Is the same for the Web server and the Socket.IO server
+var appListenOnPortConfig = process.env.PORT || 8080;
 
-app.listen(config.port, function () {
-  console.log('Express server listening on port ' + config.port);
+
+// Binding the listening socket
+var server = app.listen(appListenOnPortConfig, function () {
+  console.log('Express server listening on port ' + appListenOnPortConfig);
 });
 
+// Socket.IO will listen on the same port as our Web server
+var sio = require('socket.io').listen(server);
+
+// Static pages (such as angularjs, css and client-side js) are statically served
+app.use('/', express.static(__dirname + '/app'));
+
+var tabVotes = [
+{
+	'name': 'yes',
+	'count': 0
+},
+{
+	'name': 'no',
+	'count': 0
+},
+{
+	'name': 'dont know',
+	'count': 0
+}
+];
+
+sio.sockets.on('connection', function (socket){
+	socket.on('registerResult', function(index){
+		tabVotes[index].count++;
+		sio.sockets.emit('sendResult', tabVotes);
+	});
+});
--- public/js/tweb_myscript.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('tweb_app',[]);
-
-app.controller("pollCtrl", function($scope){
-    //...
-    
-});
