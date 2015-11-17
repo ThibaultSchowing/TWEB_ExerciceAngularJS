b1487ba068165860a79e4009ed3ccbd665fa58b1
Thibault Schowing <thibault.schowing@heig-vd.ch>
2015-11-17 16:21:19 +0100

    ça marche mais c'est pas encore beau

--- app/res/js/custom/main (1).js
+++ app/res/js/custom/main (1).js
@@ -1,4 +1,4 @@
-var tweb = angular.module('tweb', ['ngRoute', 'ngAnimate','chart.js']);
+var tweb = angular.module('tweb', ['ngRoute', 'ngAnimate']);
 
 tweb.config(['$routeProvider',
         function($routeProvider) {
@@ -24,55 +24,36 @@ tweb.config(['$routeProvider',
                 });
         }]);
 
-
+		
 tweb.controller('home', function($scope, serveurSync) {
 	$scope.datas = "";
-  $scope.labels = [];
-  $scope.data = [];
-//données affichée sur la page home.html
+	
 	serveurSync.registerCbs(function(datas){
 		$scope.datas = datas;
-
-    var tabLabels = [];
-    var tabValues = [];
-
-    for (var i = 0; i < datas.length; i++){
-      tabLabels.push(datas[i].name);
-      tabValues.push(datas[i].count);
-    }
-
-    $scope.labels = tabLabels;
-    $scope.data = tabValues;
-
-
 	});
-
+	
 	$scope.vote = function(index){
 		serveurSync.voter(index);
 	};
-
-
-
 });
 
 tweb.factory('serveurSync', function(){
 	var sock = io.connect();
 	var _cbResults = null;
-
-// on recoit le tableau complet du serveur
+	
 	sock.on('sendResult', function(datas){
-		if (_cbResults !== null)
+		if (_cbResults !== null) 
 			_cbResults(datas);
 	});
-
+	
 	var _voter = function(index){
 		sock.emit('registerResult', index);
 	};
-
+	
 	var _registerCbs = function(cbResults) {
 		_cbResults = cbResults;
 	};
-
+	
 	return {
 		voter: _voter,
 		registerCbs: _registerCbs
